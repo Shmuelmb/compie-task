2403aea25b2fa04e1ca84256e9e7c4eb901eaa23
shmuelmb <>
2025-01-14 09:55:12 +0200

    minor changes

--- backend/src/api/controllers/health.ts
+++ backend/src/api/controllers/health.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express"
-import { responseWithError } from "@/status/status"
+import { responseWithError, responseWithSuccess } from "@/status/status"
 import * as services from "@/services/"
 
 export const getHealthCheck = async (req: Request, res: Response) => {
   try {
-    return services.getHealth()
+    return responseWithSuccess(res, await services.getHealth())
   } catch (err) {
     return responseWithError(res, err)
   }
--- backend/src/api/services/health.ts
+++ backend/src/api/services/health.ts
@@ -3,7 +3,7 @@ import { BAD_GATEWAY_MSG, BAD_REQUEST } from "@/status/codes"
 
 export const getHealth = async () => {
   try {
-    return success({ status: "ok" })
+    return success("", "Oh yeh we are healthy")
   } catch (err) {
     throw error(err.message || BAD_GATEWAY_MSG, err.code || BAD_REQUEST)
   }
